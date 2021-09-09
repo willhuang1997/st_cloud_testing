82743176a048fa8483dad42d33118995d9ba0dc9
willhuang1997 <willhuang1997@gmail.com>
2021-09-09 15:51:02 -0700

    Changing App

--- debug.py
+++ debug.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-menu_options = {}
-menu_options = {'Get Help': '', 'Report a bug': "https://google.com", 'About': "HELLOOOOO"}
-st.set_page_config(menu_options=menu_options)
+menu_items = {}
+menu_items = {'Get Help': 'google.com', 'Report a bug': "https://google.com", 'About': "*HELLOOOOO*"}
+st.set_page_config(menu_items=menu_items)
