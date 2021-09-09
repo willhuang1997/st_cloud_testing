7c99146ecd1b0ebe06b76d9862d166a1b2f9641c
willhuang1997 <willhuang1997@gmail.com>
2021-09-09 15:53:00 -0700

    Changing App

--- debug.py
+++ debug.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 menu_items = {}
-menu_items = {'Get Help': 'google.com', 'Report a bug': "https://google.com", 'About': "*HELLOOOOO*"}
+menu_items = {'Get Help': 'https://google.com', 'Report a bug': "https://google.com", 'About': "*HELLOOOOO* # This is a header!!!!"}
 st.set_page_config(menu_items=menu_items)
