d33067f46458ff62730021ecdedcbf9f49788691
willhuang1997 <willhuang1997@gmail.com>
2021-09-14 18:06:32 -0700

    Change about

--- debug.py
+++ debug.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 menu_items = {}
-menu_items = {'Get Help': 'https://google.com', 'Report a bug': "https://google.com", 'About': "*HELLOOOOO* # This is a header!!!!"}
+menu_items = {'Get Help': 'https://google.com', 'Report a bug': "https://google.com", 'About': "*HELLOOOOO* # This is a header!!!ffffdsafdsafdsafdsafdkasfjd;klja;ljeripwqrie;lwj;kjefiodjz;olfdsjkshfidos;lajflkhsaiufds;lkanfjldkhsafiodslj;anfhsdl;iaoew;jlkqjflidojf;alkndjlhaiuo;jlfknjewlfhojei;klanjfldsiuhfio;sjdlkanjfkbdsaljfil;dsk,anfljksdlj;kan,fjdsakl!"}
 st.set_page_config(menu_items=menu_items)
