cff96392033e4899502bc5075e43934b2be49ba9
willhuang1997 <willhuang1997@gmail.com>
2021-09-14 20:03:14 -0700

    Change about

--- debug.py
+++ debug.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-menu_items = {}
-menu_items = {'Get Help': 'https://google.com', 'Report a bug': "https://google.com", 'About': "*HELLOOOOO* # This is a header!!!ffffdsafdsafdsafdsafdkasfjd;klja;ljeripwqrie;lwj;kjefiodjz;olfdsjkshfidos;lajflkhsaiufds;lkanfjldkhsafiodslj;anfhsdl;iaoew;jlkqjflidojf;alkndjlhaiuo;jlfknjewlfhojei;klanjfldsiuhfio;sjdlkanjfkbdsaljfil;dsk,anfljksdlj;kan,fjdsakl!"}
+menu_items = {'Get Help': "https://www.streamlit.io", "Report a bug": "https://www.google.com/help", "About" : "Hello. This App was built by William Huang. As a software Engineer, I have been working for a few years as a software engineer at different companies. One of the companies that I have worked at is Cisco. Another company that I have worked at is LogMeIn. These companies are different in size and in culture. Currently, I work at Streamlit and Streamlit is the best!"}
 st.set_page_config(menu_items=menu_items)
