410cc075183413c28c1acfbbbb705edd64e7a741
William Huang <whuang@streamlit.io>
2021-11-30 15:14:04 -0800

    Testing

--- debug.py
+++ debug.py
@@ -1,26 +1,11 @@
 import streamlit as st
 
-user_input = st.text_input("Enter Movie Name")
-st.info(user_input)
+#st.set_page_config(layout="wide")
 
-#st.markdown(" ## Hamburger Menu Customization!")
+st.write("Hello World")
+st.text_input('Hello World')
+screenshot = st.camera_input('Hello World', 'Testing1')
 
-#st.write("Here is the new, original hamburger menu without customization in developer mode.")
-#st.image("original.png")
-#st.markdown("**As one can see, there is now a developer menu in grey in developer mode!**")
+if screenshot:
+    st.image(screenshot)
 
-#st.write("Here is the new, original hamburger menu without customization as a viewer.")
-#st.image("original_viewer.png")
-
-#st.write(" If one wants to show information about their app, one can do that through this feature!")
-#st.image("about.jpeg", "This is an example!")
-
-#st.code("""about_info = ''' ## My Custom App This app uses our ML model to demostrate churn prediction '''
-#menu_items = {'About': about_info}
-#st.set_page_config(menu_items=menu_items)""")
-
-#st.write("If one wants to change the link, one can do that through the 'Get help' or 'Report a bug' key")
-#st.code("""
-#get_help_link = https://support.google.com/googlenest/troubleshooter/7211062?hl=en
-#menu_items = {'Get help': get_help_link}
-#st.set_page_config(menu_items=menu_items) """)
