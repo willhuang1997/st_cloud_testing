10f76bf3ed8bf267f4bc2f1ad28222ee03142a69
willhuang1997 <willhuang1997@gmail.com>
2023-02-22 10:15:17 -0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -5,3 +5,5 @@ import streamlit.components.v1 as components
 components.iframe("https://docs.streamlit.io/en/latest")
 
 components.iframe("https://streamlit-demo-self-driving-streamlit-app-8jya0g.streamlit.app/?embed=true")
+
+components.iframe("https://resolve-labs-feed-back-feedback-input-lv24tg.streamlit.app/?embed=true")
