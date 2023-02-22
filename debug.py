8ef0d6c544ae80dab4c15c81d311fc1c06430085
willhuang1997 <willhuang1997@gmail.com>
2023-02-22 10:11:10 -0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 # embed streamlit docs in a streamlit app
 components.iframe("https://docs.streamlit.io/en/latest")
 
-components.iframe("https://streamlit-demo-self-driving-streamlit-app-8jya0g.streamlit.app/?embedded=true")
+components.iframe("https://streamlit-demo-self-driving-streamlit-app-8jya0g.streamlit.app/?embed=true")
