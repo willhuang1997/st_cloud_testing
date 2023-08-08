14841b66f895e142abc337664f56379018b1929f
willhuang1997 <willhuang1997@gmail.com>
2023-08-08 15:11:36 -0700

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,9 +1,30 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-# embed streamlit docs in a streamlit app
-components.iframe("https://docs.streamlit.io/en/latest")
-st.write("---")
-components.iframe("https://streamlit-demo-self-driving-streamlit-app-8jya0g.streamlit.app/?embed=true")
-st.write("---")
-components.iframe("https://resolve-labs-feed-back-feedback-input-lv24tg.streamlit.app/?embed=true", height = 1000)
+
+if "logged_in" not in st.session_state:
+    form = st.form(key="my_form")
+
+    form.subheader("Login")
+
+    user = form.text_input("Username")
+    password = form.text_input("Password", type="password", autocomplete="new-password")
+
+    if form.form_submit_button("Login"):
+        # Check if username and password are correct
+
+        st.session_state["user"] = user
+        st.session_state["password"] = password
+        st.session_state["logged_in"] = True
+        st.experimental_rerun()
+
+
+else:
+    st.subheader("Logged in")
+    st.write("You are logged in.")
+
+    st.write("username:", st.session_state["user"])
+    st.write("password:", st.session_state["password"])
+
+    if st.button("Logout"):
+        del st.session_state["logged_in"]
+        st.experimental_rerun()
